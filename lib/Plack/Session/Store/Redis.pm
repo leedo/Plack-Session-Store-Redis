6764720003f48ed9bd0b2159d878240053a83cdc
Lee Aylward <lee@laylward.com>
2013-10-07 23:03:20 -0400

    increase version to 0.04

--- lib/Plack/Session/Store/Redis.pm
+++ lib/Plack/Session/Store/Redis.pm
@@ -15,11 +15,11 @@ Plack::Session::Store::Redis - Redis based session store for Plack apps.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
